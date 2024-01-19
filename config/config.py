ea04e0f9e4cf9a4ce1c1f7e1d21a0a4471aecf7f
Cw Song <109223193+S-SIRIUS@users.noreply.github.com>
2024-01-19 16:12:17 +0900

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,5 +1,5 @@
 import os
-hive_api_key = 'O+hgLkPUCzoDCFB+8TJQ7YGnjzbCLNnZ'
-cortex_api_key = 'FrfLo5S0DZT7p1VR0vvoJ0gK8z60tfJv'
-OPENAI_API_KEY = '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'
+hive_api_key = 'your_api'
+cortex_api_key = 'your_api'
+OPENAI_API_KEY = 'your_api'
 os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
