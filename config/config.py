e9c761e771ae6a332234f45419aae859a2fe0f12
Cw Song <109223193+S-SIRIUS@users.noreply.github.com>
2024-01-19 21:28:54 +0900

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,6 +1,6 @@
 import os
-hive_api_key = 'O+hgLkPUCzoDCFB+8TJQ7YGnjzbCLNnZ'
-cortex_api_key = 'FrfLo5S0DZT7p1VR0vvoJ0gK8z60tfJv'
-OPENAI_API_KEY = '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'
+hive_api_key = ''
+cortex_api_key = ''
+OPENAI_API_KEY = ''
 os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
-google_pwd = "zvuh ksid rrza wqfy"
+google_pwd = ""
